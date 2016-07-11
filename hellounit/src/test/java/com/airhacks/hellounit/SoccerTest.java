b1824e80951ecbb206c4857cef19bbc8bfd8b76f
Adam Bien <abien@adam-bien.com>
2016-07-11 10:17:39 +0200

    Better exception testing with Rules

--- hellounit/src/test/java/com/airhacks/hellounit/SoccerTest.java
+++ hellounit/src/test/java/com/airhacks/hellounit/SoccerTest.java
@@ -1,7 +1,10 @@
 package com.airhacks.hellounit;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
  *
@@ -11,6 +14,9 @@ public class SoccerTest {
 
     private Soccer cut;
 
+    @Rule
+    public ExpectedException expected = ExpectedException.none();
+
     @Before
     public void init() {
         this.cut = new Soccer();
@@ -18,6 +24,13 @@ public class SoccerTest {
 
     @Test(expected = IllegalStateException.class)
     public void isExciting() {
+        this.cut.isExciting();
+    }
+
+    @Test
+    public void isExcitingWrongQuestion() {
+        expected.expect(RuntimeException.class);
+        expected.expectMessage(containsString("question"));
         this.cut.isExciting();
     }
 
