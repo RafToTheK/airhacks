edbfb6f19e557bd7cd378f74db7a29a4ddb822ad
Adam Bien <abien@adam-bien.com>
2016-07-11 11:38:54 +0200

    refactoring to JsonObject

---
+++ sports/src/main/java/com/airhacks/sports/boundary/SportsCatalog.java
@@ -0,0 +1,16 @@
+package com.airhacks.sports.boundary;
+
+/**
+ *
+ * @author airhacks.com
+ */
+public class SportsCatalog {
+
+    public String all(String category) {
+        if (category == null) {
+            throw new IllegalArgumentException("Category parameter cannot be null");
+        }
+        return "go, chess";
+    }
+
+}
--- sports/src/main/java/com/airhacks/sports/boundary/SportsResource.java
+++ sports/src/main/java/com/airhacks/sports/boundary/SportsResource.java
@@ -1,7 +1,12 @@
 package com.airhacks.sports.boundary;
 
+import javax.inject.Inject;
+import javax.json.Json;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 /**
  *
@@ -10,9 +15,16 @@ import javax.ws.rs.Path;
 @Path("sports")
 public class SportsResource {
 
+    @Inject
+    SportsCatalog catalog;
+
     @GET
-    public String all() {
-        return "fishing, chess, bowling, soccer";
+    public Response all(@Context UriInfo info) {
+        return Response.ok(Json.createObjectBuilder().
+                add("sports", catalog.all(info.getPath())).
+                add("info", info.getPath()).
+                build()).build();
+
     }
 
 }
